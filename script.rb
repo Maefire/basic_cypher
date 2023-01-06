23452fac971a2aaf1f93fd770a9bbcd0154c2b11
MaeFire <masked_phoenix@yahoo.com>
2023-01-06 02:40:25 -0600

    Add method for string validation|display output
    I needed to make sure that the sentence structure was kept. If it was
    lower-case, upper-case, or a non-letter, then it would shift and return
    properly. I then needed to show the actual outcome. As a bonus, I
    also showed the original string.

--- script.rb
+++ script.rb
@@ -10,3 +10,25 @@ shift_amount = gets.chomp.to_i
 def shift_str_char(char, base, shift_amount)
     (((char.ord - base) + shift_amount) % 26 + base).chr
 end
+
+## USED TO KEEP SENTENCE STRUCTURE (symbols, capitalization,etc...) ##
+def str_char_validation(user_string,shift_amount)
+    result = ""
+    user_string.each_char do |char|
+        case char
+        when "a".."z"
+            result << shift_str_char(char, "a".ord, shift_amount)
+        when "A".."Z"
+            result << shift_str_char(char, "A".ord, shift_amount)
+        else 
+            result << char
+        end
+    end
+    result
+end
+
+## RETURN ENCRYPTION AND ORIGINAL ##
+encrypted = str_char_validation(user_string, shift_amount)
+puts("Thank you! Your encrypted sentence is:\n#{encrypted}")
+original_sentence = str_char_validation(encrypted, shift_amount)
+puts("Your original sentence was:\n#{original_sentence}")
